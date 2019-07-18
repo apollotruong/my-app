c5c2ef11875351e9ce2ca3c277167e9c58563007
Apollo Truong <atruo017@ucr.edu>
2019-07-18 10:54:55 -0700

    added profile pic

--- src/App.js
+++ src/App.js
@@ -1,72 +1,45 @@
-import React from 'react';
-import './App.css';
-import { Component } from 'react';
-import { SocialIcon } from 'react-social-icons';
-import Radium from 'radium';
-import {StyleRoot} from 'radium';
-import { fadeIn } from 'react-animations';
-import {ProfilePicture} from './components/profile-pic';
-
-
+import React from "react";
+import "./App.css";
+import { Component } from "react";
+import { SocialIcon } from "react-social-icons";
+import Radium from "radium";
+import { StyleRoot } from "radium";
+import { fadeIn } from "react-animations";
+import { ProfilePicture } from "./components/profile-pic";
+import picture from "./assets/profilePicture.jpg";
+// import "./assets/profile-pic.css";
 
 class App extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      hovered: false
-    }
-  }
-
-  mouseEnter = () => {
-    this.setState({hovered: true})
-  }
-  mouseLeave = () => {
-    this.setState({hovered: false})
-  }
-
-  render(){
-    const classNames = ['test'];
-    if (this.state.hovered) {
-      classNames.push('hovered')
-    }
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-        
           Apollo Truong
-          <p onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className={classNames.join(' ')}>
-            Hey!
-
-            
-
-            
-          </p>
+          <p className="test">Hey!</p>
+          <ProfilePicture imageSrc={picture} />
           <StyleRoot>
-          <div className="test" style={styles.fadeIn}>
-          <SocialIcon network="linkedin" bgColor="#00f6ff" url="https://www.linkedin.com/in/apollo-truong-a61129116/" style={styles.socialIconLinkedIn}> </SocialIcon>
-          <SocialIcon network="email" bgColor="#00f6ff" url="mailto:apollomtruong@gmail.com"> </SocialIcon>
-          <ProfilePicture />
-          <SocialIcon network="github" bgColor="#00f6ff" url="https://github.com/apollotruong" ></SocialIcon>
-          </div>
+            <div style={styles.fadeIn}>
+              <SocialIcon
+                network="linkedin"
+                bgColor="#00f6ff"
+                url="https://www.linkedin.com/in/apollo-truong-a61129116/"
+              />
+              <SocialIcon network="email" bgColor="#00f6ff" url="mailto:apollomtruong@gmail.com" />
+              <SocialIcon network="github" bgColor="#00f6ff" url="https://github.com/apollotruong" />
+            </div>
           </StyleRoot>
-
-        </header>  
-          
-        
+        </header>
       </div>
-
-      
     );
   }
 }
 
 export default App;
 
-
 const styles = {
   fadeIn: {
-    animation: 'x 2s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+    animation: "x 2s",
+    animationName: Radium.keyframes(fadeIn, "fadeIn")
   },
 
   socialIconLinkedIn: {
--- src/components/profile-pic.jsx
+++ src/components/profile-pic.jsx
@@ -1,26 +1,39 @@
-import * as React from 'react';
+import * as React from "react";
 
 export class ProfilePicture extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {hovered: false};
-    }
-    mouseEnter = () => {
-        console.log('hello', this)
-        this.setState({hovered: true})
-    }
-    mouseLeave = () => {
-        this.setState({hovered: false})
-    }
-    render() {
-        if (this.state.hovered) {
-            return null;
-        }
-        return (
-            <div className="comp-profile-pic" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
-
-                <img src={this.props.imageSrc} alt={'Pic Belongs Here'} onMouseEnter={this.onMouseEnter}/>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hovered: false };
+  }
+  mouseEnter = () => {
+    console.log("hello", this);
+    this.setState({ hovered: true });
+  };
+  mouseLeave = () => {
+    this.setState({ hovered: false });
+  };
+  render() {
+    // if (this.state.hovered) {
+    //   return null;
+    // }
+    return (
+      <div className="comp-profile-pic" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+        <img
+          src={this.props.imageSrc}
+          alt={"Pic Belongs Here"}
+          onMouseEnter={this.onMouseEnter}
+          style={profileStyles.circular}
+        />
+      </div>
+    );
+  }
 }
+
+const profileStyles = {
+  circular: {
+    width: "200px",
+    height: "200px",
+    overflow: "hidden",
+    borderRadius: "50%"
+  }
+};
